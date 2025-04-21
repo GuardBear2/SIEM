c2bf513722958890f1951e4dcf416e95efdb6a4b
nikinov <novellenicholas@gmail.com>
2025-04-21 17:41:10 +0200

    renaming phase 3

--- tools/mitre/mitredb.py
+++ tools/mitre/mitredb.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-# Copyright (C) 2015, Wazuh Inc.
-# Created by Wazuh, Inc. <info@wazuh.com>.
+# Copyright (C) 2015, GuardBear Inc.
+# Created by GuardBear, Inc. <info@guardbear.com>.
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
 #
 # Example:
--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (C) 2015, Wazuh Inc.
+# Copyright (C) 2015, GuardBear Inc.
 
 # validate OS, linux or macos
 if [ "X$(uname)" = "XLinux" ] ; then
